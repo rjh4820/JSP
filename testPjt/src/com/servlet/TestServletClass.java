07eb9f1db133533fac7a9e7d55f8815d31bb9cd8
rjh4820 <rjh4820@hanmail.net>
2020-01-21 16:39:43 +0900

    20200121_LifeCycle

--- testPjt/src/com/servlet/TestServletClass.java
+++ testPjt/src/com/servlet/TestServletClass.java
@@ -1,6 +1,9 @@
 package com.servlet;
 
 import java.io.IOException;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,12 +18,28 @@ public class TestServletClass extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
-		
+		System.out.println("doGet()");
 		
 	}
-
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
 	}
-
+	
+	@PostConstruct
+	public void funPc() {
+		System.out.println("@PostConstruct");
+	}
+	@Override
+	public void init() throws ServletException {
+		System.out.println("init()");
+	}
+	
+	@Override
+	public void destroy() {
+		System.out.println("destroy()");
+	}
+	@PreDestroy
+	public void funPd() {
+		System.out.println("@PreDestroy");
+	}
 }
